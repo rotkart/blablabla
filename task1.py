56de86c2c3f5c2aaaeb41016277a44e77d1270e2
rotkart <rotkart@gmail.com>
2023-11-24 12:05:22 +0300

    Solution for first subtask

--- task1.py
+++ task1.py
@@ -4,5 +4,4 @@ with open("students.csv") as data_file:
     stud_data = reader(data_file, delimiter=',')
     for item in stud_data:
         if "Хадаров Владимир" in item[1]:
-            print(item)
-    print(stud_data.line_num)
+            print(f"Ты получил: {item[4]}, за проект - {item[2]}")
